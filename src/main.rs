eee73ae081fbb1605b6167dd952e2ca39f79d7d8
Aleksei Sukontsev <aleksei.sukontsev@gmail.com>
2018-05-25 17:35:15 +0200

    Number of threads computed as max(1, number_of_cpus - 1) instead of hardcoded value.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate image;
 extern crate rand;
 extern crate rayon;
+extern crate num_cpus;
 
 use template::flame_template::FlameTemplate;
 use std::time::Instant;
@@ -12,10 +13,13 @@ pub mod transforms;
 pub mod output;
 pub mod util;
 
+const PRESERVE_CPUS : u32 = 1;
+
 fn main() {
     let now = Instant::now();
+    let threads = (num_cpus::get() as u32 - PRESERVE_CPUS).max(1);
     let template: FlameTemplate = example::spark();
-    let renderer = render::multithreaded_renderer::Renderer { threads: 7 };
+    let renderer = render::multithreaded_renderer::Renderer { threads };
     let raw = renderer.render(&template);
 //    let raw = render::simple_renderer::render(&template);
     output::write("fractal.png", raw, &template.render);
