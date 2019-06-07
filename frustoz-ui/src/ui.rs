6f6a6914fd735733bea0a73865f482e4f31af811
Aleksei Sukontsev <aleksei.sukontsev@gmail.com>
2019-06-07 02:03:56 +0200

    Added combobox for selecting fractal for rendering from examples.

--- frustoz-ui/src/ui.rs
+++ frustoz-ui/src/ui.rs
@@ -39,16 +39,20 @@ pub fn build_ui(application: &gtk::Application) {
         });
     }
 
-    let button = gtk::Button::new_with_label("Click me!");
+    let example_selector = gtk::ComboBoxText::new();
+    example_selector.append_text(SPARK_STR);
+    example_selector.append_text(SIERPINSKY_STR);
+    example_selector.append_text(BARNSLEY_STR);
+
     let dr = drawing.clone();
     {
         let mut sta = Arc::clone(&state);
-        button.connect_clicked(move |x| {
+        example_selector.connect_changed(move |x| {
             let st = &mut sta.lock().unwrap();
-            on_click(x, st, dr.clone());
+            on_select(x, st, dr.clone());
         });
     }
-    bx.add(&button);
+    bx.add(&example_selector);
     bx.add(&drawing);
     window.show_all();
 }
@@ -57,8 +61,12 @@ pub fn build_ui(application: &gtk::Application) {
 fn render_flame(flame: &Flame) -> Pixbuf {
     let threads = (num_cpus::get() as u32 - crate::PRESERVE_CPUS).max(1);
     let renderer = render::multithreaded_renderer::Renderer { threads };
+    let mut f = flame.clone();
+    f.render.width = 1024;
+    f.render.height = 768;
+    f.render.quality = 100;
 
-    let raw = renderer.render::<render::NoOpReporter>(flame.clone());
+    let raw = renderer.render::<render::NoOpReporter>(f);
     let raw_bytes = Bytes::from(&raw);
     Pixbuf::new_from_bytes(&raw_bytes, Colorspace::Rgb, false, 8, 1024, 768, 3 * 1024)
 }
@@ -67,9 +75,17 @@ fn init_state() -> UIState {
     UIState { flame: None, raw: None }
 }
 
-fn on_click(button: &gtk::Button, state: &mut UIState, dr: gtk::DrawingArea) {
-    state.flame = Some(frustoz_core::example::spark());
-    state.raw = Some(render_flame(state.flame.as_ref().unwrap()));
+fn on_select(example_selector: &gtk::ComboBoxText, state: &mut UIState, dr: gtk::DrawingArea) {
+    use Example::*;
+    let id = example_selector.get_active_text().map(|i| i.to_string());
+    let example = id.as_ref().map(|x| &**x) // Converting Option<String> to Option<&str> never fails to amuse me
+        .and_then(Example::get_example);
+    state.flame = example.map(|x| match  x {
+        Spark => frustoz_core::example::spark(),
+        Sierpinsky => frustoz_core::example::sierpinsky(),
+        Barnsley => frustoz_core::example::barnsley(),
+    });
+    state.raw = state.flame.as_ref().map(|flame| render_flame(flame));
     dr.queue_draw();
 }
 
@@ -79,4 +95,37 @@ pub fn draw(draw: &gtk::DrawingArea, c: &cairo::Context, state: &mut UIState) {
         c.set_source_pixbuf(&buf, 0f64, 0f64);
         c.paint();
     });
+}
+
+enum Example {
+    Spark,
+    Sierpinsky,
+    Barnsley,
+}
+
+const SPARK_STR: &str = "spark";
+const SIERPINSKY_STR: &str = "sierpinsky";
+const BARNSLEY_STR: &str = "barnsley";
+
+
+impl Example {
+
+    fn get_text(&self) -> &str {
+        use Example::*;
+        match self {
+            Spark => SPARK_STR,
+            Sierpinsky => SIERPINSKY_STR,
+            Barnsley => BARNSLEY_STR,
+        }
+    }
+
+    fn get_example(id: &str) -> Option<Example> {
+        use Example::*;
+        match id {
+            SPARK_STR => Some(Spark),
+            SIERPINSKY_STR => Some(Sierpinsky),
+            BARNSLEY_STR => Some(Barnsley),
+            _ => None,
+        }
+    }
 }
