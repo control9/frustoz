0e41f0402331a8c78cd6472405623002cc887be3
control9 <aleksei.sukontsev@gmail.com>
2025-04-28 18:03:06 +0200

    Sharing for progressive renderer

--- frustoz-core/src/render/progressive_render_task.rs
+++ frustoz-core/src/render/progressive_render_task.rs
@@ -1,21 +1,22 @@
+use std::sync::Arc;
+use std::sync::atomic::Ordering::Relaxed;
 use crate::model::builders;
 use crate::model::flame::Flame;
 use crate::render::canvas::Camera;
-use crate::render::progressive_renderer::{SingleThreadSnapshot, TaskCommand};
 use crate::render::{Canvas};
 use crate::util::math::RealPoint;
 use rand::{rng, Rng};
 use tokio_with_wasm::alias as tokio;
 use tokio::sync::mpsc::error::TryRecvError;
 use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::task::yield_now;
+use crate::render::progressive_renderer::CanvasState;
 
 pub struct ProgressiveRenderTask {
     camera: Camera,
-    canvas: Canvas,
+    canvas: Arc<CanvasState>,
     flame: Flame,
-    id: usize,
-    rx: Receiver<TaskCommand>,
-    tx: Sender<SingleThreadSnapshot>,
+    id: usize
 }
 
 const SKIP_ITERATIONS: u64 = 20;
@@ -23,65 +24,45 @@ const SKIP_ITERATIONS: u64 = 20;
 impl ProgressiveRenderTask {
     pub(crate) fn new(
         flame: Flame,
-        rx: Receiver<TaskCommand>,
+        canvas: Arc<CanvasState>,
         id: usize,
-        tx: Sender<SingleThreadSnapshot>,
     ) -> Self {
         let camera = builders::camera(&flame.camera);
-        let canvas = builders::canvas(&flame.render, flame.filter.width);
         ProgressiveRenderTask {
             camera,
             canvas,
             flame,
             id,
-            rx,
-            tx,
         }
     }
 
-    pub(crate) fn render(&mut self) {
-        let mut rng = rng();
-        let xstart: f64 = rng.random_range(0.0..1.0);
-        let ystart: f64 = rng.random_range(0.0..1.0);
+    pub(crate) async fn render(&mut self) -> () {
+        
+        let xstart: f64 = rng().random_range(0.0..1.0);
+        let ystart: f64 = rng().random_range(0.0..1.0);
         let mut point = RealPoint(xstart, ystart);
-        let mut color: f64 = rng.random_range(0.0..1.0);
+        let mut color: f64 = rng().random_range(0.0..1.0);
 
-        let mut complete = false;
         let mut iteration = 0;
         info!("Task {} started", self.id);
-        while !complete {
-            let transform_seed: f64 = rng.random_range(0.0..1.0);
-            let transform = self.flame.transforms.get_transformation(transform_seed);
+        loop {
+            let transform = {
+                let transform_seed: f64 = rng().random_range(0.0..1.0);
+                self.flame.transforms.get_transformation(transform_seed)
+            };
 
-            let (new_point, new_color) = transform.apply(&point, color, &mut rng);
+            let (new_point, new_color) = transform.apply(&point, color, &mut rng());
             point = new_point;
             color = new_color;
             iteration += 1;
 
             if iteration > SKIP_ITERATIONS {
                 let camera_coordinates = self.camera.project(&point);
-                self.canvas
+                self.canvas.canvas
                     .project_and_update(&camera_coordinates, self.flame.palette.get_color(color));
             }
-
-            match self.rx.try_recv() {
-                Ok(TaskCommand::FrameExpected) => {
-                    let canv = self.canvas.clone();
-                    let snapshot = SingleThreadSnapshot {
-                        histogram: canv,
-                        steps: iteration,
-                    };
-                    self.tx.blocking_send(snapshot).unwrap();
-                }
-                Ok(TaskCommand::Completed) => {
-                    complete = true;
-                }
-                Err(TryRecvError::Disconnected) => {
-                    error!("Sender for TaskCommand disconnected before sending Completed");
-                    complete = true;
-                }
-                Err(TryRecvError::Empty) => {} // Nothing to do
-            }
+            _ = self.canvas.steps.fetch_add(1, Relaxed);
+            yield_now().await;
         }
     }
 }
